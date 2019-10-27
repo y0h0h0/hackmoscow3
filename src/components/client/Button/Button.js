252a368ea136078b374796430b60ca24381d97bd
Tom Teaser <y0h0h0@yandex.ru>
2019-10-27 05:06:29 +0300

    clearing

--- src/components/client/Button/Button.js
+++ src/components/client/Button/Button.js
@@ -1,34 +1,13 @@
 import React from 'react';
 import './Button.scss';
 
-const Button = ({ type, disabled, linkurl, onClick, text, className, tabIndex, minwidth, size = 'medium', icon = '' }) => {
-
-  let classes = [className||''];
-  switch(type) {
-    case 'secondary': classes.push('secondary'); break;
-    case 'ghost': classes.push('ghost'); break;
-    default: classes.push('primary');
-  }
-
-  if(disabled) classes.push('disabled');
-  if(text) classes.push('withText');
-  if(icon) classes.push(`withIcon icon_${icon}`); // icon = `icon_${icon}`;}
-  if(size) classes.push(`size_${size}`);
-
+const Button = ({ onClick, text }) => {
 
   return <div className="c-grButton" onClick={onClick}>
     <div/>
     <div>{text}</div>
     <div/>
   </div>
-
-  //
-  // button
-  //
-  //
-  // return <button onClick={onClick} className={`button ${classes.join(' ')}`} style={{minWidth:minwidth||null}} tabIndex={tabIndex} >
-  //           {text}
-  //         </button>;
 
 
 }
--- src/components/client/Input/Input.js
+++ src/components/client/Input/Input.js
@@ -1,24 +1,12 @@
 import React from 'react';
 import './Input.scss';
 
-
-// import { getIntAsString, getFloatAsString } from 'core/functions';
-
 const Input = (props) => {
   const getFocus = (e) => {
     if(e.target.tagName === 'DIV') e.target.getElementsByTagName('INPUT')[0].focus();
   }
 
   const handleChange = (e) => {
-    // let newVal = e.target.value;
-    // switch(props.valueType) {
-    //   case 'int':
-    //       newVal = getIntAsString(newVal);
-    //       break;
-    //   case 'float':
-    //       newVal = getFloatAsString(newVal);
-    //       break;
-    // }
     props.onChange(e.target.value)
   }
 
